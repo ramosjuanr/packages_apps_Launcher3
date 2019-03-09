1cf7f05fefa3021304deb589c33224435ecae37a
Juan <jramos69@gmail.com>
2019-03-09 13:33:10 -0500

    Revert "Introduce Utils & Fix Workspace"
    This reverts commit b7e9bc48288274bb4f1e2194b6b9e293811f4bfe.

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -57,7 +57,7 @@ import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.Toast;
 
-import com.android.launcher3.util.Utils;
+import com.android.internal.util.du.Utils;
 
 import com.android.launcher3.Launcher.LauncherOverlay;
 import com.android.launcher3.LauncherAppWidgetHost.ProviderChangedListener;
